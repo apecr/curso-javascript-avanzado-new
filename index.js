fc81ec5b2b09f4357ca3339d5f323cbcb60512c6
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-05-22 20:36:46 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+
+module.exports = function enigma(fn){
+  var slice = Array.prototype.slice;
+  var args = slice.call(arguments, 1);
+  return function(){
+    var newargs = slice.call(arguments);
+    return fn.apply(this, args.concat(newargs));
+  };
+}
---
+++ test/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const enigma = require('../index');
+const expect = require('chai').expect;
+
+const saluda = function(nombre){
+  return `Hola ${nombre} !`;
+};
+
+const saluda2 = function(saludo, nombre){
+  return `${saludo} ${nombre} !`;
+};
+
+const saludaWithThis = function(saludo){
+  return `${saludo}, ${this.nombre} !`;
+};
+
+describe('Enigma function', () => {
+  it('Two params invoked', () => {
+    var cosa = enigma(saluda, "Mundo");
+    expect(cosa()).to.equal('Hola Mundo !');
+  });
+  it('Three params invoked', () => {
+    var cosa = enigma(saluda2, "Hola", "Mundo");
+    expect(cosa()).to.equal('Hola Mundo !');
+  });
+  it('Three params invoked with this involved', () => {
+    var dario = {nombre: 'Dario'};
+    var cosa = enigma(saludaWithThis, "Que pasa");
+    //Aqui si podemos asignar el contexto dario a la funcion saludaWithThis
+    expect(cosa.call(dario)).to.equal('Que pasa, Dario !');
+  });
+});
