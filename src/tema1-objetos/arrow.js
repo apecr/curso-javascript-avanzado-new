ad7ffb2910f9e4fdd0aa8a700c011867b0859ec8
apecr <alberto.eyo@gmail.com>
2020-04-26 11:13:07 +0200

    feat() tema 1 files

---
+++ src/tema1-objetos/arrow.js
@@ -0,0 +1,51 @@
+const sumador = {
+  resultado: 0,
+  sumar: function(numeros) {
+    numeros.forEach((n) => {
+      this.resultado += n
+    })
+    return this.resultado
+  },
+  getThisResultado: function() {
+    return () => {
+      this.resta = 0
+      return this
+    }
+  },
+}
+
+// not strict mode
+const arguments = 42; 
+const arr = () => arguments;
+
+const triangulo = {
+    a: { x: 1, y: 1 },
+    b: { x: 2, y: 2 },
+    c: { x: 3, y: 1 },
+    area: function () {
+      return Math.abs(
+        (this.b.x - this.a.x) * (this.b.y - this.a.y) -
+          ((this.c.x - this.a.x) * (this.c.y - this.a.y)) / 2
+      );
+    },
+    areaArrow: () => {
+      return Math.abs(
+        (this.b.x - this.a.x) * (this.b.y - this.a.y) -
+          ((this.c.x - this.a.x) * (this.c.y - this.a.y)) / 2
+      );
+    },
+    areaArrowIFFE: function () {
+      return (() => {
+        return Math.abs(
+          (this.b.x - this.a.x) * (this.b.y - this.a.y) -
+            ((this.c.x - this.a.x) * (this.c.y - this.a.y)) / 2
+        );
+      })();
+    },
+  };
+
+  module.exports = {
+      triangulo,
+      arr,
+      sumador
+  }
--- src/tema1-objetos/symbol.js
+++ src/tema1-objetos/symbol.js
@@ -1,10 +1,5 @@
 const miSimbolo = Symbol('Julian')
-
-console.log(Symbol.keyFor(Symbol.for('Julian')))
-
 const obj = {}
 obj[miSimbolo] = 'Julian'
-console.log(JSON.stringify(obj))
-console.log(Object.keys(obj))
-console.log(Object.getOwnPropertyNames(obj))
-console.log(Object.getOwnPropertySymbols(obj))
+
+module.exports = obj
